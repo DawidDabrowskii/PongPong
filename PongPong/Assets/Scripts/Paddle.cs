8dd648b00d8929964d486f3003452b1b009251a8
DawidDabrowskii <dabrowskidawid680@gmail.com>
2022-07-30 16:13:14 +0200

    1.0

---
+++ PongPong/Assets/Scripts/Paddle.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    protected Rigidbody2D _rigidbody;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+}
